ae3b2bbe88d6b76fd25d9165da7d36c7e1b03707
KeumSangHo <keumm9612@naver.com>
2024-11-23 23:00:35 +0900

    style: 라이트하우스 성능 향상

--- src/components/TableHead.tsx
+++ src/components/TableHead.tsx
@@ -10,12 +10,22 @@ const TableHead = () => {
 
   return (
     <p className={styles['calendar-head']}>
-      <span onClick={() => setMinusChooseMonth()}>이전 달</span>
+      <span
+        className={styles['calendar-head-btn']}
+        onClick={() => setMinusChooseMonth()}
+      >
+        이전 달
+      </span>
       <span>
         {year}년 {month}
       </span>
 
-      <span onClick={() => setPlusChooseMonth()}>다음 달</span>
+      <span
+        className={styles['calendar-head-btn']}
+        onClick={() => setPlusChooseMonth()}
+      >
+        다음 달
+      </span>
     </p>
   );
 };
